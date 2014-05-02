33fa493b880088484a93e1e2c9988467205ba299
Andrew Nelson <andy@andyhub.com>
2014-05-02 23:22:16 +0200

    Got article text

--- context.js
+++ context.js
@@ -1,6 +1,14 @@
 /*
 TODO:
-1. Find main story body <section>
-2. Search <section> for keyword dictionary matches
-3. Add link between matches and something interesting
+1. Search <section> for keyword dictionary matches
+2. Add link between matches and something interesting
 */
+
+var articleText;
+
+window.document.onload = function(e)
+{
+    var articleDiv = document.getElementById("article-body-blocks");
+    articleText = articleDiv.textContent || articleDiv.innerText;
+    articleText = articleText.trim();
+};
